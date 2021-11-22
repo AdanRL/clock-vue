42319e43df75f679c51856c1038a72083e86ac19
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-11-22 20:35:49 +0000

    First commit 🚀

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<VueClock></VueClock>
+<VueClock></VueClock>
+<VueClock></VueClock>
+</template>
+
+<script>
+import VueClock from "./components/VueClock.vue";
+export default {
+
+  name: "App",
+
+  components: {
+
+     VueClock: VueClock
+
+  }
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/VueClock.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="clock-container">
+    <h1>Hello Clock</h1>
+    <div class="clock">
+      {{ hours }}:{{ minutes }}:{{ seconds }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"VueClock",
+  data() {
+    return {
+      hours: "--",
+      minutes: "--",
+      seconds: "--"
+    }
+  },
+  methods: {
+    getDate() {
+      const f = new Date();
+      this.hours = this.pad(f.getHours())
+      this.minutes = this.pad(f.getMinutes())
+      this.seconds = this.pad(f.getSeconds())
+    },
+    pad(value) {
+      return String(value).padStart(2,"0")
+    }
+  },
+  mounted() {
+
+    setInterval(() => this.getDate(), 1000)
+  }
+
+}
+
+</script>
+
+<style>
+  body {
+
+    margin: auto;
+
+}
+
+.clock-container {
+
+    background: black;
+    color:white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+
+}
+
+.clock {
+
+  background: black;
+  height: 60px;
+  color: red;
+  font-size: 20px;
+  text-shadow: 0 0 1px rgb(201, 124, 124), 
+  0 0 5px rgba(201, 124, 124),
+  0 0 10px rgba(201, 124, 124), 
+  0 0 20px rgb(161, 72, 72),
+  0 0 40px rgba(161, 72, 72), 
+  0 0 60px rgba(161, 72, 72),
+  0 0 80px rgba(161, 72, 72), 
+  0 0 120px rgba(161, 72, 72),
+  0 0 150px rgba(161, 72, 72);
+}
+</style>
